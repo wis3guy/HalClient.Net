6ea708d3c35ca09295cc58f363498793b3d40ae5
Geoffrey Braaf <geoffrey@braaf.nl>
2015-10-30 13:32:25 +0100

    fixed some issues with awaiting the cached api root; Tabs to spaces;

--- HalClient.Net/HalHttpClientFactoryBase.cs
+++ HalClient.Net/HalHttpClientFactoryBase.cs
@@ -4,54 +4,69 @@ using HalClient.Net.Parser;
 
 namespace HalClient.Net
 {
-	public abstract class HalHttpClientFactoryBase
-	{
-		protected IHalJsonParser Parser;
-		private IRootResourceObject _cachedApiRootResource;
+    public abstract class HalHttpClientFactoryBase
+    {
+        protected IHalJsonParser Parser;
+        private IRootResourceObject _cachedApiRootResource;
 
-		protected HalHttpClientFactoryBase(IHalJsonParser parser)
-		{
-			if (parser == null)
-				throw new ArgumentNullException("parser");
+        protected HalHttpClientFactoryBase(IHalJsonParser parser)
+        {
+            if (parser == null)
+                throw new ArgumentNullException("parser");
 
-			Parser = parser;
-		}
+            Parser = parser;
+        }
 
-		protected IRootResourceObject GetApiRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
-		{
-			if (config.ApiRootResourceCachingBehavior != CachingBehavior.Never)
-			{
-				switch (config.ApiRootResourceCachingBehavior)
-				{
-					case CachingBehavior.Once:
-						return GetCachedRootResource(client, config);
-					case CachingBehavior.PerClient:
-						return GetFreshRootResource(client, config);
-				}
-			}
+        protected IRootResourceObject GetApiRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
+        {
+            if (config.ApiRootResourceCachingBehavior == CachingBehavior.Never)
+                return null;
 
-			return null;
-		}
+            switch (config.ApiRootResourceCachingBehavior)
+            {
+                case CachingBehavior.Once:
+                    return GetCachedRootResource(client, config);
+                case CachingBehavior.PerClient:
+                    return GetFreshRootResource(client, config);
+                default:
+                    throw new NotSupportedException($"Unsupported caching behavior: {config.ApiRootResourceCachingBehavior}");
+            }
+        }
 
-		private IRootResourceObject GetCachedRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
-		{
-			_cachedApiRootResource = _cachedApiRootResource ?? GetFreshRootResource(client, config);
+        private IRootResourceObject GetCachedRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
+        {
+            _cachedApiRootResource = _cachedApiRootResource ?? GetFreshRootResource(client, config);
 
-			return _cachedApiRootResource;
-		}
+            return _cachedApiRootResource;
+        }
 
-		private static IRootResourceObject GetFreshRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
-		{
-			if (config.BaseAddress == null)
-				throw new InvalidOperationException("The root resource can only be requested for caching if the BaseAddress of the client is initialized in the Configure method of the factory.");
+        private static IRootResourceObject GetFreshRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
+        {
+            if (config.BaseAddress == null)
+                throw new InvalidOperationException(
+                    "The root resource can only be requested for caching if the BaseAddress of the client is initialized in the Configure method of the factory.");
 
-			IRootResourceObject resource = null;
+            try
+            {
+                IRootResourceObject resource = null;
 
-			client.GetAsync(config.BaseAddress)
-				.ContinueWith(x => resource = x.Result, TaskContinuationOptions.NotOnFaulted)
-				.Wait();
+                // Opted to await here, rather than in the consuming code of the derived concrete factory. 
+                // Motivation is that we will not *always* have to communicate with the API upon 
+                // client creation; depends on IHalHttpClientConfiguration.ApiRootResourceCachingBehavior.
 
-			return resource;
-		}
-	}
+                var task = client
+                    .GetAsync(config.BaseAddress)
+                    .ContinueWith(x => resource = x.Result, TaskContinuationOptions.NotOnFaulted);
+
+                task.ConfigureAwait(false);
+                task.Wait();
+
+                return resource;
+            }
+            catch (AggregateException e)
+            {
+                throw new Exception("Failed to retreive a fresh API root resource", e);
+            }
+        }
+    }
 }

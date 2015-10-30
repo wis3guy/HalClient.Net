49607cbec6de440c20c1829a0f64d991230d1da4
Geoffrey Braaf <geoffrey@braaf.nl>
2015-10-30 17:07:11 +0100

    Updated nuget.exe and modified build.cmd to use msbuild from VS2015

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,7 @@
+rem Build with Visual Studio's version of MSBuild that understands C# 6
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" HalClient.Net.sln /p:Configuration=Release /p:Platform="Any CPU"
 cd HalClient.Net
 copy ..\ReadMe.md ReadMe.txt
-..\.nuget\nuget.exe Pack HalClient.Net.csproj -Prop Configuration=Release -build
+rem Pack without actually doing a build
+..\.nuget\nuget.exe Pack HalClient.Net.csproj -Prop Configuration=Release
 cd ..

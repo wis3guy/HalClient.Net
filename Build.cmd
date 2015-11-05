2161ad1c7ed67d839a565036d088aa1b920ec7c8
Geoffrey Braaf <geoffrey@braaf.nl>
2015-11-05 09:12:15 +0100

    modified nuget packaging

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,4 @@
-rem Build with Visual Studio's version of MSBuild that understands C# 6
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" HalClient.Net.sln /p:Configuration=Release /p:Platform="Any CPU"
 cd HalClient.Net
-copy ..\ReadMe.md ReadMe.txt
-rem Pack without actually doing a build
 ..\.nuget\nuget.exe Pack HalClient.Net.csproj -Prop Configuration=Release
 cd ..

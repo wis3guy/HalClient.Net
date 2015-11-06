c6d8f36a256fcec42f16c7150d9549e769d064f3
Geoffrey Braaf <geoffrey@braaf.nl>
2015-11-06 11:39:14 +0100

    initial commit

--- HalClient.Net/HalHttpClientFactory.cs
+++ HalClient.Net/HalHttpClientFactory.cs
@@ -39,47 +39,73 @@ namespace HalClient.Net
 			return new HttpClient(httpMessageHandler);
 		}
 
-		protected IRootResourceObject GetApiRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
-		{
-			if (config.ApiRootResourceCachingBehavior != CachingBehavior.Never)
-			{
-				switch (config.ApiRootResourceCachingBehavior)
-				{
-					case CachingBehavior.Once:
-						return GetCachedRootResource(client, config);
-					case CachingBehavior.PerClient:
-						return GetFreshRootResource(client, config);
-				}
-			}
-
-			return null;
-		}
-
-		private IRootResourceObject GetCachedRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
-		{
-			_cachedApiRootResource = _cachedApiRootResource ?? GetFreshRootResource(client, config);
-
-			return _cachedApiRootResource;
-		}
-
-		private static IRootResourceObject GetFreshRootResource(IHalHttpClient client, IHalHttpClientConfiguration config)
+		private static async Task<IRootResourceObject> GetFreshRootResourceAsync(IHalHttpClient client, IHalHttpClientConfiguration config)
 		{
 			if (config.BaseAddress == null)
 				throw new InvalidOperationException("The root resource can only be requested for caching if the BaseAddress of the client is initialized in the Configure method of the factory.");
 
-			var task = client.GetAsync(config.BaseAddress);
-			task.ConfigureAwait(false);
-			task.Wait();
-
-			return task.Result;
+			return await client.GetAsync(config.BaseAddress).ConfigureAwait(false);
 		}
 
 		public IHalHttpClient CreateClient()
 		{
-			return CreateClient(GetHttpClient());
+			return CreateHalHttpClient(GetHttpClient()).Decorated;
 		}
 
 		public IHalHttpClient CreateClient(HttpClient httpClient)
+		{
+			if (httpClient == null)
+				throw new ArgumentNullException(nameof(httpClient));
+
+			return CreateHalHttpClient(httpClient).Decorated;
+		}
+
+		public IHalHttpClient CreateClient(HttpMessageHandler httpMessageHandler)
+		{
+			if (httpMessageHandler == null)
+				throw new ArgumentNullException(nameof(httpMessageHandler));
+
+			return CreateHalHttpClient(GetHttpClient(httpMessageHandler)).Decorated;
+		}
+
+		public async Task<IHalHttpClient> CreateClientAsync(CachingBehavior apiRootCachingBehavior)
+		{
+			return await CreateHalHttpClientAsync(GetHttpClient(), apiRootCachingBehavior);
+		}
+
+		public async Task<IHalHttpClient> CreateClientAsync(HttpMessageHandler httpMessageHandler, CachingBehavior apiRootCachingBehavior)
+		{
+			if (httpMessageHandler == null)
+				throw new ArgumentNullException(nameof(httpMessageHandler));
+
+			return await CreateHalHttpClientAsync(GetHttpClient(httpMessageHandler), apiRootCachingBehavior);
+		}
+
+		public async Task<IHalHttpClient> CreateClientAsync(HttpClient httpClient, CachingBehavior apiRootCachingBehavior)
+		{
+			return await CreateHalHttpClientAsync(httpClient, apiRootCachingBehavior);
+		}
+
+		private async Task<IHalHttpClient> CreateHalHttpClientAsync(HttpClient httpClient, CachingBehavior apiRootCachingBehavior)
+		{
+			var created = CreateHalHttpClient(httpClient);
+
+			switch (apiRootCachingBehavior)
+			{
+				case CachingBehavior.PerClient:
+					var apiRootResource = await GetFreshRootResourceAsync(created.Decorated, created.Wrapped);
+					created.Wrapped.CachedApiRootResource = apiRootResource;
+					break;
+				case CachingBehavior.Once:
+					_cachedApiRootResource = _cachedApiRootResource ?? await GetFreshRootResourceAsync(created.Decorated, created.Wrapped);
+					created.Wrapped.CachedApiRootResource = _cachedApiRootResource;
+					break;
+			}
+
+			return created.Decorated;
+		}
+
+		private CreatedHalHttpClient CreateHalHttpClient(HttpClient httpClient)
 		{
 			if (httpClient == null)
 				throw new ArgumentNullException(nameof(httpClient));
@@ -90,11 +116,7 @@ namespace HalClient.Net
 			{
 				Configure(halHttpClient);
 
-				var transformed = Decorate(halHttpClient);
-
-				halHttpClient.CachedApiRootResource = GetApiRootResource(transformed, halHttpClient);
-
-				return transformed;
+				return new CreatedHalHttpClient(Decorate(halHttpClient), halHttpClient);
 			}
 			catch (Exception)
 			{
@@ -102,14 +124,24 @@ namespace HalClient.Net
 				throw;
 			}
 		}
+	}
 
-		public IHalHttpClient CreateClient(HttpMessageHandler httpMessageHandler)
+	internal class CreatedHalHttpClient
+	{
+		public CreatedHalHttpClient(IHalHttpClient decorated, HalHttpClient wrapped)
 		{
-			if (httpMessageHandler == null)
-				throw new ArgumentNullException(nameof(httpMessageHandler));
+			if (decorated == null)
+				throw new ArgumentNullException(nameof(decorated));
 
-			return CreateClient(GetHttpClient(httpMessageHandler));
+			if (wrapped == null)
+				throw new ArgumentNullException(nameof(wrapped));
+
+			Decorated = decorated;
+			Wrapped = wrapped;
 		}
+
+		public IHalHttpClient Decorated { get; }
+		public HalHttpClient Wrapped { get; }
 	}
 
 	public abstract class HalHttpClientFactory<T> : HalHttpClientFactory, IHalHttpClientFactory<T>
@@ -118,15 +150,9 @@ namespace HalClient.Net
 		{
 		}
 
-		protected virtual void Configure(IHalHttpClientConfiguration config, T context)
-		{
-			// Do nothing by default ...
-		}
+		protected abstract void Configure(IHalHttpClientConfiguration config, T context);
 
-		protected virtual IHalHttpClient Decorate(IHalHttpClient original, T context)
-		{
-			return original; // return original by default ...
-		}
+		protected abstract IHalHttpClient Decorate(IHalHttpClient original, T context);
 
 		public IHalHttpClient CreateClient(T context)
 		{
@@ -144,11 +170,11 @@ namespace HalClient.Net
 			{
 				Configure(halHttpClient, context);
 
-				var transformed = Decorate(halHttpClient, context);
+				var decorated = Decorate(halHttpClient, context);
 
-				halHttpClient.CachedApiRootResource = GetApiRootResource(transformed, halHttpClient);
+				halHttpClient.CachedApiRootResource = GetApiRootResource(decorated, halHttpClient);
 
-				return transformed;
+				return decorated;
 			}
 			catch (Exception)
 			{
--- HalClient.Net/IHalHttpClientFactory.cs
+++ HalClient.Net/IHalHttpClientFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HalClient.Net
 {
@@ -7,6 +8,10 @@ namespace HalClient.Net
 		IHalHttpClient CreateClient();
 		IHalHttpClient CreateClient(HttpClient httpClient);
 		IHalHttpClient CreateClient(HttpMessageHandler httpMessageHandler);
+
+		Task<IHalHttpClient> CreateClientAsync(CachingBehavior apiRootCachingBehavior);
+		Task<IHalHttpClient> CreateClientAsync(HttpClient httpClient, CachingBehavior apiRootCachingBehavior);
+		Task<IHalHttpClient> CreateClientAsync(HttpMessageHandler httpMessageHandler, CachingBehavior apiRootCachingBehavior);
 	}
 
 	public interface IHalHttpClientFactory<in T> : IHalHttpClientFactory
@@ -14,5 +19,9 @@ namespace HalClient.Net
 		IHalHttpClient CreateClient(T context);
 		IHalHttpClient CreateClient(HttpClient httpClient, T context);
 		IHalHttpClient CreateClient(HttpMessageHandler httpMessageHandler, T context);
+
+		Task<IHalHttpClient> CreateClientAsync(CachingBehavior apiRootCachingBehavior, T context);
+		Task<IHalHttpClient> CreateClientAsync(HttpClient httpClient, CachingBehavior apiRootCachingBehavior, T context);
+		Task<IHalHttpClient> CreateClientAsync(HttpMessageHandler httpMessageHandler, CachingBehavior apiRootCachingBehavior, T context);
 	}
 }

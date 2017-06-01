ba6ede9084bcbb7fb8fe61e0c7a3bad9e66cee09
Geoffrey Braaf <geoffrey@braaf.nl>
2017-06-01 14:31:35 +0200

    bumped the version number

--- HalClient.Net/Properties/SharedAssemblyInfo.cs
+++ HalClient.Net/Properties/SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("Geoffrey Braaf, WIS3GUY.NET")]
 [assembly: AssemblyProduct("HalClient.Net")]
 [assembly: AssemblyCopyright("Copyright (C) Geoffrey Braaf, WIS3GUY.NET")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyInformationalVersion("3.0.0")]

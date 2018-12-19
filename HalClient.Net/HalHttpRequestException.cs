6df51a80b96570c9af4b7a43175a2442390b7d46
Mogens Heller Grabe <mookid8000@gmail.com>
2018-12-19 13:01:38 +0100

    target .net standard 2.0 and add serialization ctor

--- HalClient.Net/HalHttpRequestException.cs
+++ HalClient.Net/HalHttpRequestException.cs
@@ -1,20 +1,27 @@
 using System;
 using System.Net;
+using System.Runtime.Serialization;
 using HalClient.Net.Parser;
 
 namespace HalClient.Net
 {
-	[Serializable]
-	public class HalHttpRequestException : Exception
-	{
-		public HalHttpRequestException(HttpStatusCode statusCode, string reason, IRootResourceObject resource = null)
-			: base($"{(int) statusCode} ({reason})")
-		{
-			StatusCode = statusCode;
-			Resource = resource;
-		}
+    [Serializable]
+    public class HalHttpRequestException : Exception
+    {
+#if REQUIRES_EXCEPTION_SERIALIZATION_CTOR
+        protected HalHttpRequestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+#endif
 
-		public HttpStatusCode StatusCode { get; }
-		public IRootResourceObject Resource { get; }
-	}
+        public HalHttpRequestException(HttpStatusCode statusCode, string reason, IRootResourceObject resource = null)
+            : base($"{(int)statusCode} ({reason})")
+        {
+            StatusCode = statusCode;
+            Resource = resource;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+        public IRootResourceObject Resource { get; }
+    }
 }
